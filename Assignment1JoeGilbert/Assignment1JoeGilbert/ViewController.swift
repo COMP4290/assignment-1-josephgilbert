9613d5e2addd82cee59847b229d243c63129e5ea
Joe <joe@Joes-Mac.local>
2019-01-15 13:04:56 -0500

    Initial check in

---
+++ Assignment1JoeGilbert/Assignment1JoeGilbert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment1JoeGilbert
+//
+//  Created by Joe on 1/11/19.
+//  Copyright © 2019 Joe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
